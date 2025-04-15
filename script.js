fc06d366df871d2b1625c448ee58420251475742
Nouneimchik <wadymy@gmail.com>
2025-04-15 22:55:02 +0300

    push3

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            const response = await fetch(`https://steam-coop-site.netlify.app/getSteamInfo/${appId}`);
+            const response = await fetch(`https://steam-coop-site.netlify.app/netlify/functions/getSteamInfo/${appId}`);
             if (!response.ok) throw new Error('Помилка запиту до сервера!');
 
             const game = await response.json();
